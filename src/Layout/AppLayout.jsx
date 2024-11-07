675c836cf3c8679267ea2404df0299d85929f1cd
remeessalam <remeesreme4u@gmail.com>
2024-11-07 15:16:50 +0530

    footer also completed home page completed

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const AppLayout = () => {
   return (
@@ -11,6 +12,7 @@ const AppLayout = () => {
             <Outlet />
           </div>
         </div>
+        <Footer />
       </div>
     </>
   );
