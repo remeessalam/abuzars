f4420187d2b440cb7dc057cb8d753d1e7efc6894
remeessalam <remeesreme4u@gmail.com>
2024-11-08 16:27:22 +0530

    all completed

--- src/pages/ContactusPage/ContactusPage.jsx
+++ src/pages/ContactusPage/ContactusPage.jsx
@@ -1,9 +1,11 @@
 import Section from "./Sections/Section";
+import SectionTwo from "./Sections/SectionTwo";
 
 const ContactusPage = () => {
   return (
     <>
       <Section />
+      <SectionTwo />
     </>
   );
 };
