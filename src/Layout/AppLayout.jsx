28c566185c0e9c63296ce7e197dd2884325cb79a
remeessalam <remeesreme4u@gmail.com>
2024-11-10 23:07:34 +0530

    added water mark on screen

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -2,13 +2,20 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import ScrollToTop from "./ScrollTop";
+import Logo from "../assets/svg/Logo.png";
 
 const AppLayout = () => {
   return (
     <>
       <div className="flex flex-col justify-center  relative ">
         <Header />
-        <div className="relative top-28 left-0">
+        <div className="relative top-28 left-0 bg-contain bg-center bg-no-repeat">
+          <div className="fixed flex justify-center items-center opacity-20 -z-10 top-0 bg-contain bg-center bg-no-repeat w-screen  h-screen">
+            <div
+              className="w-1/2 h-full bg-contain bg-center bg-no-repeat"
+              style={{ backgroundImage: `url(${Logo})` }}
+            />
+          </div>
           <div className="overflow-hidden">
             <ScrollToTop />
             <Outlet />
