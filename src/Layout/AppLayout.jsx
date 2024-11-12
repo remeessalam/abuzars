daa00c2b4ad0b2169a18d1e049502120a6118649
remeessalam <remeesreme4u@gmail.com>
2024-11-12 10:34:57 +0530

    bg image change to video

--- src/Layout/AppLayout.jsx
+++ src/Layout/AppLayout.jsx
@@ -3,8 +3,10 @@ import Header from "./Header";
 import Footer from "./Footer";
 import ScrollToTop from "./ScrollTop";
 import Logo from "../assets/svg/Logo.png";
+import bgvideo from "../assets/video/backgroundvideo.mp4";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ReactPlayer from "react-player";
 const AppLayout = () => {
   return (
     <>
@@ -26,9 +28,18 @@ const AppLayout = () => {
         <Header />
         <div className="relative top-28 left-0 bg-contain bg-center bg-no-repeat">
           <div className="fixed flex justify-center items-center opacity-20 -z-10 top-0 bg-contain bg-center bg-no-repeat w-screen  h-screen">
-            <div
-              className="w-1/2 h-full bg-contain bg-center bg-no-repeat"
-              style={{ backgroundImage: `url(${Logo})` }}
+            {/* <div
+              className="w-full h-full bg-center bg-no-repeat"
+              // style={{ backgroundImage: `url(${Logo})` }}
+            /> */}
+            <ReactPlayer
+              url={bgvideo}
+              loop={true}
+              playsinline
+              playing={true}
+              width="100%"
+              height="100%"
+              muted
             />
           </div>
           <div className="overflow-hidden">
